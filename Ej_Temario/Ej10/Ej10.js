859dd598493b0b4f85d543aa99ae2d40d566f1e9
vicolores <151919535+vicolores@users.noreply.github.com>
2024-10-18 10:07:49 +0000

    update

--- Ej_Temario/Ej10/Ej10.js
+++ Ej_Temario/Ej10/Ej10.js
@@ -5,9 +5,18 @@ palíndromo complejo: "La ruta nos aporto otro paso natural"
 */
 function esPalindromo(cadena) {
   // Convertir todo a minúsculas y eliminar espacios
+  // La expresión /[^a-z0-9]/g es una expresión regular que define el patrón que queremos buscar.
+  // / y /: Estos delimitadores indican el inicio y el final de la expresión regular.
+  // En este caso, estamos buscando cualquier carácter que no sea una letra minúscula o un dígito.
+  // El signo ^ indica que estamos buscando caracteres que no estén en el conjunto de caracteres especificado.(niega el conjunto de caracteres)
+  // El signo g indica que la búsqueda debe ser global, es decir, que se debe buscar en toda la cadena de texto, no solo en la primera ocurrencia.
+  // En resumen, [^a-z0-9] significa cualquier carácter que no sea una letra minúscula o un número, lo que se reemplaza por una cadena vacía "" en la cadena original.
   let cadenaLimpia = cadena.toLowerCase().replace(/[^a-z0-9]/g, "");
 
   // Revertir la cadena limpia
+  // .split("") convierte la cadena en un array de caracteres
+  // .reverse() invierte el orden de los elementos del array
+  // .join("") une los elementos del array en una sola cadena(lo contrario que .split(""))
   let cadenaRevertida = cadenaLimpia.split("").reverse().join("");
 
   // Verificar si la cadena original limpia es igual a la revertida
